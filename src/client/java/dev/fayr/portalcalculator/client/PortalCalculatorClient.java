051694c171dbda5ed8c3a1ba2c941b1511ee41ed
GFayrr <199396723+GFayrr@users.noreply.github.com>
2026-04-19 22:35:04 +0200

    Initial commit - Portal Calculator 1.20.3

---
+++ src/client/java/dev/fayr/portalcalculator/client/PortalCalculatorClient.java
@@ -0,0 +1,10 @@
+package dev.fayr.portalcalculator.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PortalCalculatorClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
